d05d59f1dd3980d86cf860692a748b74d59112ee
jmalabed <jmalabed27@gmail.com>
2022-06-04 11:58:50 -0700

    new location for serviceworker file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import PushScript from "./components/PushScript";
 
 function App() {
   return (
     <div className="App">
       <h1>Swelter</h1>
-      <img src="https://cdn-icons-png.flaticon.com/512/879/879247.png" />
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/879/879247.png"
+        alt="buoy"
+      />
+      <PushScript />
     </div>
   );
 }
