90c0ce8566c90bcd8f3b67c3b5ea40be7bc1c9e9
jmalabed <jmalabed27@gmail.com>
2022-06-04 11:52:39 -0700

    updated OneSignal file location

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import PushScript from "./components/PushScript";
+
 function App() {
   return (
     <div className="App">
       <h1>Swelter</h1>
       <img src="https://cdn-icons-png.flaticon.com/512/879/879247.png" />
-      <PushScript />
     </div>
   );
 }
