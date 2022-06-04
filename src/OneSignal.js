594fd6aa732db7063a0174961b4fcd798f111b90
jmalabed <jmalabed27@gmail.com>
2022-06-04 12:43:08 -0700

    updated function export

--- src/OneSignal.js
+++ src/OneSignal.js
@@ -1,9 +1,11 @@
 import OneSignal from "react-onesignal";
 
-export default async function runOneSignal() {
+const runOneSignal = async () => {
   await OneSignal.init({
     appId: "e7bc5c09-8ad7-4473-aeaf-03c0ac4d512e",
     allowLocalhostAsSecureOrigin: true,
   });
   OneSignal.showSlidedownPrompt();
-}
+};
+
+export default runOneSignal;
